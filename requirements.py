a077250aa0eb6180dc935fcfd0b158553d69bce4
Iumy Pimentel <117225271+Iuky-O@users.noreply.github.com>
2024-11-09 17:31:43 -0300

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,7 +1,8 @@
-Flask==2.0.3
-Flask-CORS==3.0.10
-tensorflow==2.7.0
-numpy==1.21.2
-Pillow==8.3.2
-opencv-python-headless==4.5.3.56
-gdown==4.2.0
+Flask
+Flask-CORS
+tensorflow
+numpy
+Pillow
+opencv-python
+gunicorn
+gdown
